b6e64bead5a996226839e4a92082999215794de8
RickOliverRatas <162234935+RickOliverRatas@users.noreply.github.com>
2024-09-06 09:17:13 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo : "Momodora: Moonlit Farewell",
+        descricao : "Momodora: Moonlit Farewell é o último lançamento da série Momodora. Junte-se a Momo, alta sacerdotisa da vila de Koho, conforme ela embarca numa missão divina para salvar seu povo da destruição iminente pelas mãos de hordas de demônios",
+        link : "https://store.steampowered.com/app/1747760/Momodora_Moonlit_Farewell/",
+        tags: "animes anime indie lua sacerdotismo"
+    },
+    {
+        titulo : "Dandy Ace",
+        descricao : "Dandy Ace é um jogo de ação extravagante, que segue um fabuloso mágico buscando combinar e usar suas cartas mágicas, lutando e saqueando para derrotar o Ilusionista de Olhos Verdes e se libertar de um espelho amaldiçoado.",
+        link : "https://store.steampowered.com/app/1037130/Dandy_Ace/",
+        tags: "carta magia magica"
+    },
+    {
+        titulo : "SUPER CHICKEN JUMPER",
+        descricao : "Super Chicken Jumper: Uma aventura emplumada e explosiva! Super Chicken Jumper é um jogo frenético que combina elementos de plataforma e tiro, resultando em uma experiência única e divertida. Neste game, você assume o papel de uma galinha-espiã com uma missão crucial: salvar o mundo de uma ameaça demoníaca!",
+        link : "https://store.steampowered.com/app/584640/SUPER_CHICKEN_JUMPER/",
+        tags: "galinha dinossaura animes anime gemaplys gema vitor viton"
+    }
+];
+
